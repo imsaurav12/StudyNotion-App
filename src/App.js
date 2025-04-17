78fae963cfab3d9f0ea3e55c921489e4631ba72d
Saurav Raj <er.sauravraj12@gmail.com>
2025-04-18 01:24:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ const App = () => {
         />
 
       </Routes>
+     
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const Home = ({isLoggedIn}) => {
+const Home = ({ isLoggedIn }) => {
   return (
-    <div className='flex justify-center items-center text-white text-3xl h-full'>
-      This is Home Page
-    </div>
-  )
-}
+    <div className="min-h-screen bg-richblack-900 flex flex-col justify-center items-center px-4 text-white text-center relative">
+      <h1 className="text-4xl font-bold text-yellow-50 mb-6">
+        Welcome to StudyNotion 👋
+      </h1>
 
-export default Home
+      {/* Bottom Full-Width Note */}
+      <div className="absolute bottom-0 left-0 w-full bg-richblack-800 text-richblack-200 py-3 px-6 text-sm text-center border-t border-richblack-700">
+        🛠️ Currently working on this project. Full version coming soon. Stay tuned!
+      </div>
+    </div>
+  );
+};
+
+export default Home;
